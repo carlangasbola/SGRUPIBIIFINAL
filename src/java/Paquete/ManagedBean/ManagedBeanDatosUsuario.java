3e417b44e42bd2991425a6092c7454f451c3e238
Carlangas <carlangasbola@gmail.com>
2019-07-24 21:46:37 -0500

    EditarDatosUsuario

--- src/java/Paquete/ManagedBean/ManagedBeanDatosUsuario.java
+++ src/java/Paquete/ManagedBean/ManagedBeanDatosUsuario.java
@@ -5,44 +5,37 @@ import Paquete.Beans.Mensajes;
 import Paquete.Pojos.DatosUsuario;
 import Paquete.Pojos.Roles;
 import Paquete.Pojos.Usuarios;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Named("DatosUsuario")
-@RequestScoped
-public class ManagedBeanDatosUsuario {
+@SessionScoped
+public class ManagedBeanDatosUsuario implements Serializable{
 
-    private List<DatosUsuario> datosUsuarios;
-    private DatosUsuario datosUsuario;
-    private Roles roles;
+    private List<DatosUsuario> datosUsuarios = new ArrayList<>();
+    private DatosUsuario datosUsuario = new DatosUsuario();
+    private Roles roles = new Roles();
 
     @PostConstruct
     private void init() {
-        datosUsuarios = new ArrayList<>();
-        datosUsuario = new DatosUsuario();
-        roles = new Roles();
         ObtenerDatosUsuarios();
     }
 
-    public String obtenerDatosUsuario(int idUsuario) {
-        Transaction tran = null;
-        try {
-            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-            tran = session.beginTransaction();
-            datosUsuario = ((DatosUsuario) session.get(DatosUsuario.class, Integer.valueOf(idUsuario)));
-            tran.commit();
-        } catch (Exception e) {
-            tran.rollback();
-        }
-
+    public String obtenerDatosUsuario(DatosUsuario du) {
+        this.datosUsuario = du;
         return "EditarDatosUsuario";
     }
+    
 
     public void actualizarDatosUsuario() {
         Mensajes mensaje = new Mensajes();
@@ -59,17 +52,18 @@ public class ManagedBeanDatosUsuario {
             u.setRoles((Roles) session.get(Roles.class, roles.getIdrol()));
             session.update(u);
 
-            datosUsuario.setUsuarios(u);
-            session.update(datosUsuario);
+            DatosUsuario du = new DatosUsuario();
+            du = (DatosUsuario) session.get(DatosUsuario.class, datosUsuario.getIdUsuarios());
+            du.setUsuarios(u);
+            session.update(du);
 
             tx.commit();
-            mensaje.setMessage("Usuario agregado");
+            mensaje.setMessage("Información Actualizada");
             mensaje.info();
         } catch (RuntimeException e) {
             tx.rollback();
-            mensaje.setMessage("No se puedo insertar el usuario, consulte el log para mas detalles");
+            mensaje.setMessage("No se pudo actualizar la información del usuario, consulte el log para mas detalles");
             mensaje.fatal();
-            throw e;
         }
     }
 
@@ -82,7 +76,7 @@ public class ManagedBeanDatosUsuario {
             tx = session.beginTransaction();
 
             Usuarios u = new Usuarios();
-            
+
             u.setUserLogin(datosUsuario.getCorreo());
             u.setPasssword(datosUsuario.getIdentificador());
             u.setRoles((Roles) session.get(Roles.class, roles.getIdrol()));
@@ -94,44 +88,63 @@ public class ManagedBeanDatosUsuario {
             tx.commit();
             mensaje.setMessage("Usuario agregado");
             mensaje.info();
-        } catch (RuntimeException e) {
+        } catch (HibernateException e) {
             tx.rollback();
-            mensaje.setMessage("No se puedo insertar el usuario, consulte el log para mas detalles");
+            mensaje.setMessage("No se puedo insertar el usuario, probablemente ya se encuentra registrado el correo.");
             mensaje.fatal();
-            throw e;
         }
     }
+    
+     public void EliminarUsuario(int idUsuario) {
+        Mensajes mensaje = new Mensajes();
+        Transaction transObj = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            transObj = session.beginTransaction();
+            session.delete((Usuarios) session.get(Usuarios.class, idUsuario));
+
+            transObj.commit();
+            mensaje.setMessage("Eliminado del sistema");
+            mensaje.info();
+            ObtenerDatosUsuarios();
+            
+        } catch (HibernateException exObj) {
+            if (transObj != null) {
+                transObj.rollback();
+                mensaje.setMessage("No se pudo eliminar, contacte el log de errores");
+                mensaje.warn();
+            }
+        }
+     }
 
     public List<DatosUsuario> ObtenerDatosUsuarios() {
         Transaction tran = null;
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        
         try {
             tran = session.beginTransaction();
             datosUsuarios = session.createQuery("FROM DatosUsuario").list();
             tran.commit();
-        } catch ( Exception e) {
+        } catch (Exception e) {
             tran.rollback();
-            throw e;
         }
         return datosUsuarios;
     }
 
     public List<DatosUsuario> obtenerDatosUsuarioRol(int idRol) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         List<DatosUsuario> datosUsuarios = new ArrayList();
+        
         try {
             session.beginTransaction();
-            Query query = session.createQuery("FROM DatosUsuario WHERE usuarios.roles.idRol = :idRol");
+            Query query = session.createQuery("FROM DatosUsuario WHERE usuarios.roles.idrol = :idRol");
             query.setParameter("idRol", idRol);
             datosUsuarios = query.list();
+            session.getTransaction().commit();
         } catch (Exception sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+            session.getTransaction().rollback();
+        } 
+        
         return datosUsuarios;
     }
 
@@ -150,11 +163,11 @@ public class ManagedBeanDatosUsuario {
     public void setRoles(Roles roles) {
         this.roles = roles;
     }
-    
-    
+
     public List<DatosUsuario> getDatosUsuarios() {
-        if(datosUsuarios == null)
+        if (datosUsuarios == null) {
             ObtenerDatosUsuarios();
+        }
         return datosUsuarios;
     }
 
